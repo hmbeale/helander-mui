b303077d3d3b0600dfd93501abaca87f26b1f327
hmbeale <holden.beale@gmail.com>
2019-08-14 20:31:09 -0400

    Updates

---
+++ precache-manifest.5c31179c5eea5c241d98801bbb1e803d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4caae46db85174533b459cfa38a48f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09320921ef8cb8b1d76e",
+    "url": "/static/css/2.1e78366a.chunk.css"
+  },
+  {
+    "revision": "67bc3fa636d4c90dcbc3",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "09320921ef8cb8b1d76e",
+    "url": "/static/js/2.4e0c3c11.chunk.js"
+  },
+  {
+    "revision": "67bc3fa636d4c90dcbc3",
+    "url": "/static/js/main.bb1ba933.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "55e3e1389fe0110a35abeeae61d6900a",
+    "url": "/static/media/img1.55e3e138.png"
+  },
+  {
+    "revision": "d441d62a173545953c2c8e050bdb0cbf",
+    "url": "/static/media/img1_Small.d441d62a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
